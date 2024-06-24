ba84ba3786dfd991b2c904ed234b0f41ec1293bf
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-06-24 15:11:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>hello</h2>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
